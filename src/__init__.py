0a8eeaea8bcc1d9a8f0ab7873424d1e7d1c8e5e4
Seunfunmi Adegoke <adegokeseunfunmi1999@gmail.com>
2022-01-02 14:19:26 +0100

    Implement visits count functionality

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect
 import os
 from flask_jwt_extended import JWTManager
+from werkzeug.utils import redirect
 
 from src.auth import auth
 from src.bookmarks import bookmarks
-from src.models import db
+from src.models import db, Bookmark
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -27,5 +28,14 @@ def create_app(test_config=None):
 
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
+
+    @app.get("/<short_url>")
+    def redirect_to_url(short_url):
+        bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404()
+
+        if bookmark:
+            bookmark.visits=bookmark.visits+1
+            db.session.commit()
+            return redirect(bookmark.url)
 
     return app
